c4984aaedf346bd4de5f1ca35ee9120f630ce539
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2023-10-13 18:46:18 +0100

    Added server endpoints for login and signup, installed mongodb to use for app, and express

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const collection = require('./mongo');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.post("/", async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const check = await collection.findOne({ email: email })
+
+        if (check) {
+            res.json("exist")
+        }
+        else {
+            res.json("notexist")
+        }
+
+    }
+    catch (e) {
+        res.json("fail")
+    }
+
+})
+
+
+
+app.post("/signup", async (req, res) => {
+    const { email, password } = req.body
+
+    const data = {
+        email: email,
+        password: password
+    }
+
+    try {
+        const check = await collection.findOne({ email: email })
+
+        if (check) {
+            res.json("exist")
+        }
+        else {
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch (e) {
+        res.json("fail")
+    }
+
+})
+
+
+app.listen(8081, () => {
+    console.log("Plugged in baby!");
+});

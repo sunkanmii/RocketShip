3a4859bf5b3017a066bb11245717798e36f9d2d6
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2023-11-02 15:00:17 +0100

    Added feature to allow rocket to stay afloat when the screen is tapped

--- frontend/components/Rocket.js
+++ frontend/components/Rocket.js
@@ -1,44 +1,46 @@
-import React, {forwardRef} from 'react';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import React, {  forwardRef } from 'react';
+import { StyleSheet, Text, View, Dimensions, Image, Animated, Easing } from 'react-native';
 import { useSharedState } from '../store';
 
 const rocketHeight = 64
 const rocketWidth = 64
 
-const Rocket = forwardRef(({rocketBottom, rocketLeft}, ref) => {
-    const [state, setState] = useSharedState();
-    return (
-        <View 
-        ref={ref}
-        style={{
-            display: 'flex',
-            height: 'fit-content',
-            width: rocketWidth,
-            position: 'absolute',
-            bottom: rocketBottom,
-            left: rocketLeft - (rocketLeft) + 20,
-            transition: 'all .2 ease',
-        }}>
-            <Image
+const Rocket = forwardRef(({ fallDownValue, rocketBottom }, ref) => {
+  const [state, setState] = useSharedState();
+
+
+  return (
+    <Animated.View
+      ref={ref}
+      style={{
+        display: 'flex',
+        height: 'fit-content',
+        width: rocketWidth,
+        position: 'absolute',
+        bottom: fallDownValue,
+        left: '1rem',
+        transition: 'all .2 ease',
+      }}>
+      <Image
         style={styles.tinyLogo}
         source={
-            require(`../assets/Space Pixel/${state.rocketSkin}`)
-          }
+          require(`../assets/Space Pixel/${state.rocketSkin}`)
+        }
       />
-        </View>
-    )
+    </Animated.View>
+  )
 })
 
 const styles = StyleSheet.create({
-    tinyLogo: {
-      width: rocketHeight,
-      height: '2rem',
-      resizeMode: 'cover'
-    },
-    logo: {
-      width: 66,
-      height: 58,
-    },
-  });
+  tinyLogo: {
+    width: rocketHeight,
+    height: '2rem',
+    resizeMode: 'cover'
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
 
 export default Rocket
